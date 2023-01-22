c903b1c28e0abb540969bb814568a2b661eb36b3
Madhan Chandrasekharan <32620038+gcmadhan@users.noreply.github.com>
2023-01-22 12:58:06 +0530

    Update spell.py

--- spell.py
+++ spell.py
@@ -1,21 +1,29 @@
 import enchant
+import wx
 from enchant.checker import SpellChecker
+from enchant.checker.CmdLineChecker import CmdLineChecker
 
-def check(txt):
-    
-    chek = SpellChecker("en_US")
-    chek.set_text(txt)
+def spelcheker(dict1, dict2, sent):
+    txt = [] #to store new word
+    my_dict = enchant.DictWithPWL(dict1, dict2)
+    chek = SpellChecker(my_dict)
+    chek.set_text(sent)
     for err in chek:
-        print(f'{err.word} : {err.suggest()}')
-        w=input("Enter word to replace :")
-        chek.replace(w)
-
-    return chek.get_text()
+        print(f'Error : {err.word} ')
+        sugg = err.suggest()
+        sug = dict(zip(range(len(sugg)),sugg))
         
+        print(f'How about this: {sug}')
+        a=input("Select option or enter word to replace: ")
+        if a.isnumeric():
+            chek.replace(sug[int(a)])
+        else:
+            chek.replace(a)
+            txt.append(a)
+
+    return(chek.get_text())
     
-
-
-if __name__=="__main__":
-    txt = input("Add text here: ")
-    print(f'Before : {txt} \n')
-    print(f'After : {check(txt)}')
+dict1 = "en_US"
+dict2 = "myword.txt"
+sent = "Hi ths is to chek"
+spelcheker(dict1, dict2, sent)
